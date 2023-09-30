44d83e3d813044d3bbf6cc1abe5f0559a5660c71
Dominik <hatchetcontact@gmail.com>
2023-09-30 13:17:13 +0200

    Init project and add gui

---
+++ main.py
@@ -0,0 +1,31 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton
+import sys
+
+class ChatbotWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(620, 400)
+
+        # Add chat area widget
+        self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10, 10, 480, 320)
+        self.chat_area.setReadOnly(True)
+
+        # Add the input field widget
+        self.input_field = QLineEdit(self)
+        self.input_field.setGeometry(10, 340, 480, 40)
+
+        # Add the button
+        self.button = QPushButton("Send", self)
+        self.button.setGeometry(500, 340, 100, 40)
+
+        self.show()
+
+
+class Chatbot:
+    pass
+
+app = QApplication(sys.argv)
+main_window = ChatbotWindow()
+sys.exit(app.exec())
